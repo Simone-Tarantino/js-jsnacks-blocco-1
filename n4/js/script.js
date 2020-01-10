0a6767d9b32f7e70b0cf14e90f77e8e2710d099c
Simone Tarantino <questamailnonmiserveaniente@gmail.com>
2020-01-10 13:39:57 +0100

    UPDATE

--- n4/js/script.js
+++ n4/js/script.js
@@ -2,8 +2,10 @@
 // grande Gatsby, chiedi all’utente il suo nome e comunicagli se
 // può partecipare o no alla festa.
 
+// for
+
 var partyList = ["Carl", "Lenny", "Marge", "Lisa"];
-var userName = prompt("Inserisci il tuo nome");
+var userName = prompt("Inserisci il tuo nome FOR");
 var access = false;
 for (i = 0; i < partyList.length; i++) {
   if(userName == partyList[i]) {
@@ -11,6 +13,23 @@ for (i = 0; i < partyList.length; i++) {
   }
 }
 if (access == true) {
+  alert("Il tuo nome è in lista, puoi accedere alla festa");
+} else {
+  alert("Il tuo nome non è in lista, non puoi accedere alla festa");
+}
+
+// while
+
+var partyListW = ["Carl", "Lenny", "Marge", "Lisa"];
+var userNameW = prompt("Inserisci il tuo nome WHILE");
+var j = 0;
+while (j < partyListW.length) {
+  if(userNameW == partyListW[j]) {
+    accessW = true;
+  }
+  j++;
+}
+if (accessW == true) {
   alert("Il tuo nome è in lista, puoi accedere alla festa");
 } else {
   alert("Il tuo nome non è in lista, non puoi accedere alla festa");
